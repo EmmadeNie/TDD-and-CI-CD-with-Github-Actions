980bd6e7cf8b62fd10d533c96852c1f4608ce7f4
emmad <emmadenie@gmail.com>
2021-03-23 11:42:04 +0100

    final commit

--- De-TDD-groentetuin/farm.js
+++ De-TDD-groentetuin/farm.js
@@ -1,4 +1,49 @@
-const getYieldForPlant = (crop)=> crop.yield
+const pumpkin = {
+    name: "pumpkin",
+    yield: 40,
+    factors: {
+        sun: {
+            low: -50,
+            medium: 0,
+            high: 50,
+        },rain: {
+            low: -20,
+            medium: 0,
+            high: 20,
+        },
+    },
+};
+
+const getYieldForPlant = (crop)=> {
+    //For refactoring getEnvironmentFactors dynamic
+    const environmentFactors = {
+        sun: "low",
+        ground: "medium",
+        wind: "high"
+    };
+if (crop.factors) {
+    let factors = [];
+    Object.keys(crop.factors).forEach(key=> {
+        // console.log(key, environmentFactors[key])
+        if (environmentFactors[key]){
+            let percentage = crop.factors[key][environmentFactors[key]];
+       factors.push((100 + percentage)/ 100)}
+    })
+    let yieldCrop = crop.yield
+    factors.forEach(factor => {
+        if (factor === 0) {
+            return}
+        yieldCrop *= factor
+    });
+    // console.log(yieldCrop)
+    return yieldCrop} else {
+    return crop.yield}
+
+}
+
+
+getYieldForPlant(pumpkin)
+
 const getYieldForCrop = (input)=> {
     return input.numCrops * getYieldForPlant(input.crop)
 }
@@ -9,6 +54,18 @@ const getTotalYield = (cropsObject)=> {
     });
     return totalYield}
 
-    const getCostsForCrop = ()=> {}
+    const getCostsForCrop = (input)=> input.numCrops*input.costPerCrop
 
-module.exports = {getYieldForPlant, getYieldForCrop,getTotalYield, getCostsForCrop  }
+const getRevenueForCrop = (input)=>getYieldForCrop(input)*input.pricePerKg
+
+const getProfitForCrop = (input)=> getRevenueForCrop(input) - getCostsForCrop(input)
+
+const getTotalProfit = (cropsObject)=> {
+    let totalProfit = 0
+    cropsObject.crops.forEach(crop=> {
+        totalProfit += getProfitForCrop(crop)
+    }); return totalProfit
+}
+
+
+module.exports = {getYieldForPlant, getYieldForCrop,getTotalYield, getCostsForCrop, getRevenueForCrop, getProfitForCrop, getTotalProfit  }
